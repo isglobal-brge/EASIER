95edb29e02464ae625c456ab95d54409e07dab91
dpelegri <dpelegri@gmail.com>
2020-12-24 10:06:31 +0100

    Update installer.R

--- installer.R
+++ installer.R
@@ -1,16 +1,16 @@
 # Install requirede libraries
-devtools::install_github("ilarischeinin/rasterpdf")
-devtools::install_github("guido-s/meta")
-devtools::install_version('tidyverse', version = '1.3.0')
-devtools::install_version('dplyr', version = '1.0.2')
-devtools::install_version('stringr', version = '1.4.0')
-devtools::install_version('ggplot2', version = '3.3.2')
-devtools::install_version('VennDiagram', version = '1.6.20')
-devtools::install_version('RColorBrewer', version = '1.1-2')
-devtools::install_version('reshape2', version = '1.4.4')
-devtools::install_version('ggsignif', version = '0.6.0')
-install.packages('readtext', version = '0.8.0')
-install.packages("tools")
+if (!require('rasterpdf')){ devtools::install_github("ilarischeinin/rasterpdf") }
+if (!require('meta')){ devtools::install_github("guido-s/meta") }
+if (!require('tidyverse')){ devtools::install_version('tidyverse', version = '1.3.0') }
+if (!require('dplyr')){ devtools::install_version('dplyr', version = '1.0.2') }
+if (!require('stringr')){ devtools::install_version('stringr', version = '1.4.0') }
+if (!require('ggplot2')){ devtools::install_version('ggplot2', version = '3.3.2') }
+if (!require('VennDiagram')){ devtools::install_version('VennDiagram', version = '1.6.20') }
+if (!require('RColorBrewer')){ devtools::install_version('RColorBrewer', version = '1.1-2') }
+if (!require('reshape2')){ devtools::install_version('reshape2', version = '1.4.4') }
+if (!require('ggsignif')){ devtools::install_version('ggsignif', version = '0.6.0') }
+if (!require('readtext')){ install.packages('readtext', version = '0.8.0') }
+if (!require('tools')){ install.packages("tools") }
 
 # Install brgeEnrich package from github-brge repositorie for enrichment :
 
@@ -22,9 +22,9 @@ devtools::install_github("isglobal-brge/brgeEnrich@HEAD")
 devtools::install_version('BiocManager', version = '1.30.10')
 
 # Bioconductor
-BiocManager::install( "missMethyl", ask = FALSE )
-BiocManager::install( "org.Hs.eg.db", ask = FALSE )
-BiocManager::install( "GenomicRanges", ask = FALSE )
-BiocManager::install( "rtracklayer", ask = FALSE )
-BiocManager::install( "IlluminaHumanMethylation450kanno.ilmn12.hg19", ask = FALSE )
-BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = FALSE)
+if (!require('missMethyl')){ BiocManager::install( "missMethyl", ask = FALSE ) }
+if (!require('org.Hs.eg.db')){ BiocManager::install( "org.Hs.eg.db", ask = FALSE ) }
+if (!require('GenomicRanges')){ BiocManager::install( "GenomicRanges", ask = FALSE ) }
+if (!require('rtracklayer')){ BiocManager::install( "rtracklayer", ask = FALSE ) }
+if (!require('IlluminaHumanMethylation450kanno.ilmn12.hg19')){ BiocManager::install( "IlluminaHumanMethylation450kanno.ilmn12.hg19", ask = FALSE ) }
+if (!require('IlluminaHumanMethylationEPICanno.ilm10b4.hg19')){ BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19", ask = FALSE) }
