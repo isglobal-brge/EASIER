0a9f8f9c18cfeb62467edb18cdbe1c2b478db38f
dpelegri <dpelegri@gmail.com>
2021-07-19 17:37:15 +0200

    added option to remove outliers beta boxplot

--- R/plot_betas_boxplot.R
+++ R/plot_betas_boxplot.R
@@ -5,6 +5,8 @@
 #' @param x numeric list with data for each factor
 #' @param filename, file name to write the boxplot with png format
 #' @param main optional, string with plot title
+#' @param outliers optional, boolean by default = FALSE, if true,
+#' plot_betas_boxplot function plots outliers
 #'
 #' @return Betas boxplot
 #'
@@ -12,7 +14,7 @@
 #' @import ggplot2 dplyr
 #'
 #' @export
-plot_betas_boxplot <- function(x, filename= NULL, main= NULL, ...)
+plot_betas_boxplot <- function(x, filename= NULL, main= NULL, outliers = FALSE, ...)
 {
 
    if(is.null(filename))
@@ -22,12 +24,22 @@ plot_betas_boxplot <- function(x, filename= NULL, main= NULL, ...)
       main = "BETAS Boxplot"
 
    x <- melt(x)
-   p <- ggplot2::ggplot( x, aes(x=factor(L1), y=value, color=factor(L1))) +
-      ggplot2::ggtitle( main ) +
-      ggplot2::geom_boxplot()+
-      ggplot2::theme_bw() +
-      ggplot2::theme( legend.position = "none",
-                      axis.text.x = element_text( angle=90, hjust=1, vjust=0.5))
+
+   if(outliers == FALSE){
+      p <- ggplot2::ggplot( x, aes(x=factor(L1), y=value, color=factor(L1))) +
+         ggplot2::ggtitle( main ) +
+         ggplot2::geom_boxplot(outlier.shape = NA) +
+         ggplot2::theme_bw() +
+         ggplot2::theme( legend.position = "none",
+                         axis.text.x = element_text( angle=90, hjust=1, vjust=0.5))
+   } else {
+      p <- ggplot2::ggplot( x, aes(x=factor(L1), y=value, color=factor(L1))) +
+         ggplot2::ggtitle( main ) +
+         ggplot2::geom_boxplot() +
+         ggplot2::theme_bw() +
+         ggplot2::theme( legend.position = "none",
+                         axis.text.x = element_text( angle=90, hjust=1, vjust=0.5))
+   }
 
    ggplot2::ggsave(filename, p)
 
